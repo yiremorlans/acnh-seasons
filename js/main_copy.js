e9f2c214668d6dc559a1c4c9d78e4abe1058f842
Yire Morlans <yire.m@icloud.com>
2022-07-16 23:00:39 -0400

    upload local images fix routes

--- js/main_copy.js
+++ js/main_copy.js
@@ -4,17 +4,17 @@ document.getElementById('fall').onclick = fallTime
 document.getElementById('winter').onclick = winterTime
 
 function springTime () {
-  document.querySelector('body').style.backgroundImage = "url('https://dodo.ac/np/images/4/43/NHWatering.jpg')";
+  document.querySelector('body').style.backgroundImage = "url('/img/NH_Watering.jpeg')";
 }
 
 function summerTime () {
-  document.querySelector('body').style.backgroundImage = "url('https://dodo.ac/np/images/6/64/NHFishing.jpg')";
+  document.querySelector('body').style.backgroundImage = "url('/img/NH_Fishing.jpeg')";
 }
 
 function fallTime () {
-  document.querySelector('body').style.backgroundImage = "url('https://dodo.ac/np/images/6/6d/NHBugCatching.jpg')";
+  document.querySelector('body').style.backgroundImage = "url('/img/NH_Bug_Catching.jpeg')";
 }
 
 function winterTime () {
-  document.querySelector('body').style.backgroundImage = "url('https://dodo.ac/np/images/f/f5/NHMakingSnowman.jpg')";
+  document.querySelector('body').style.backgroundImage = "url('/img/NH_Making_Snowboy.jpeg')";
 }
